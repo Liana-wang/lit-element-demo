103869bd9ee753b24c65dff7ba6c0b06e7d5a967
wang.lianyan <wang.lianyan@eisoo.com>
2021-01-26 15:58:57 +0800

    init

--- react-pro/src/Example.js
+++ react-pro/src/Example.js
@@ -1,24 +1,37 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 function Example() {
+  const checkRef = useRef(null)
+  const btnRef = useRef(null)
+
   const [data, setData] = useState([{ id: '2232', name: 'asdasd' }])
-  const [checked, setChecked] = useState(true)
 
-  const handleClick = (event) => {
-    setData([])
-    console.log('aaa', event)
+  const check = ({ detail }) => {
+    console.log('checked', detail.checked)
   }
 
-  const check = (event) => {
-    console.log(event)
+  const handleClick = ({ detail: { count } }) => {
+    setData(count % 2 === 0 ? [] : [{ id: '2232', name: 'asdasd' }])
+    console.log('count', count)
   }
+
+  useEffect(() => {
+    checkRef.current.addEventListener('litClick', check)
+    btnRef.current.addEventListener('litClick', handleClick)
+    console.log(checkRef)
+
+    return () => {
+      checkRef.current.removeEventListener('litClick', check)
+      btnRef.current.removeEventListener('litClick', handleClick)
+    }
+  }, [])
 
   return (
     <div className="item">
-      <lit-button data={JSON.stringify(data)} litClick={handleClick}>按钮</lit-button>
+      <lit-button ref={btnRef} data={JSON.stringify(data)}>按钮</lit-button>
       <div>
         基本用法：
-        <lit-checkbox litClick={check}>sdsdff</lit-checkbox>
+        <lit-checkbox ref={checkRef}>sdsdff</lit-checkbox>
       </div>
       <div>
         禁用：
